048a11584c939d5c490a1b9c6f2e6d115728f25c
Suhas Talekar <suhastalekar@STMBP1621.local>
2022-08-18 19:56:32 +0800

    Subscribe to router events to log pageviews to datalayer srvice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { DataLayerService } from './services/data-layer.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-google-tag-manager-setup';
+  constructor(private _router:Router, private _dataLayerService: DataLayerService)
+  {
+
+    this._router.events.subscribe(event=> { // subscribe to router events
+        if (event instanceof NavigationEnd) //if our event is of our interest
+        {
+            this._dataLayerService.logPageView(event.url) //call our dataLayer service's page view method to ping home with the url value.
+        }
+    });
+
+}
 }
